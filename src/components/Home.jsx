8adb5c69d2c9c84070345d68a9629ca80bef0ff6
kitakaze-riku <kitakaze.rop@gmail.com>
2023-04-12 11:18:32 +0900

    ✨ Homeコンポーネントでcssを適応

---
+++ src/components/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="homePage">
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+        <div className="postTextContainer">
+        今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。
+        </div>
+        <div className="nameAndDeleteButton">
+          <h3>riku-kitakaze</h3>
+          <button>削除</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
