94ab637e78f4da590e41589556070f350c2d5b4e
kitakaze-riku <kitakaze.rop@gmail.com>
2023-04-12 13:33:10 +0900

    ✨ firebaseから投稿記事を取得し画面に表示させる

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,33 +1,39 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Home.css";
 import { collection, getDocs } from "firebase/firestore";//{}は名前付きインポート
 import { db } from "../firebase";
 
 const Home = () => {
+  const [postList, setPostList] = useState([]);
+
   useEffect(() => {
     const getPosts = async () => {
-      const data = await getDocs(collection(db, "posts"));
+      const data = await getDocs(collection(db, "posts"));// dbへアクセス
       // 階層が深いのでdata関数を使うと取得が簡単になる
       // firebaseはdbからの値取得が難しい。
-      console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     }
     getPosts();
   }, [])
 
   return (
     <div className="homePage">
-      <div className="postContents">
-        <div className="postHeader">
-          <h1>タイトル</h1>
-        </div>
-        <div className="postTextContainer">
-          今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。今はReactの勉強中です、これから頑張ってReactエンジニアとして活躍して行きたいです。
-        </div>
-        <div className="nameAndDeleteButton">
-          <h3>riku-kitakaze</h3>
-          <button>削除</button>
-        </div>
-      </div>
+      {postList.map((post) => {
+        return (
+          <div className="postContents" key={post.id}>
+            <div className="postHeader">
+              <h1>{post.title}</h1>
+            </div>
+            <div className="postTextContainer">
+              {post.postText}
+            </div>
+            <div className="nameAndDeleteButton">
+              <h3>@{post.auther.username}</h3>
+              <button>削除</button>
+            </div>
+          </div>
+        )
+      })}
     </div>
   )
 }
